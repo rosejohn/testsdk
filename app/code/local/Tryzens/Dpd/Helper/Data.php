53dfff056ffa72566e220756567a3a021e2b65d9
John Smith <sangeethajohns@gmail.com>
2016-04-26 12:26:18 +0100

    helper added

---
+++ app/code/local/Tryzens/Dpd/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @package      Tryzens
+ * @subpackage   Dpd
+ * @category     Checkout
+ */
+
+/**
+ * Class Tryzens_Dpd_Helper_Data
+ */
+class Tryzens_Dpd_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
